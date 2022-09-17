4f740c8773a59b530c60012f0374ebf6d0d7678f
Jesse Lecy <jdlecy@gmail.com>
2022-09-17 16:09:57 -0700

    Update data.R

--- R/data.R
+++ R/data.R
@@ -4,3 +4,10 @@
 #'
 #' @description A vector with examples of raw titles. 
 NULL 
+
+#' @name tinypartvii
+#' @docType data
+#' @title Sample of 10,000 orgs (approximately 100,000 titles) as a Part VII data frame.  
+#'
+#' @description A data frame with Part VII data.  
+NULL 
