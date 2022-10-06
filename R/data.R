8e4fafc8804fd13e16cd460a32a35f3a122b474a
Jesse Lecy <jdlecy@gmail.com>
2022-10-06 16:20:31 -0700

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,3 +1,7 @@
+###
+### DEMO DATA
+###
+
 #' @name raw.titles
 #' @docType data
 #' @title Example titles. 
@@ -10,4 +14,68 @@ NULL
 #' @title Sample of 10,000 orgs (approximately 100,000 titles) as a Part VII data frame.  
 #'
 #' @description A data frame with Part VII data.  
+NULL 
+
+###
+### DISAMBIGUATION DATA
+###
+
+#' @name likely.subjects
+#' @docType data
+#' @title Helper data list.  
+#'
+#' @description Used in the `fix_of()` function.  
+NULL 
+
+#' @name likely.titles
+#' @docType data
+#' @title Helper data list.  
+#'
+#' @description Used in the...   
+NULL 
+
+#' @name possible.titles
+#' @docType data
+#' @title Helper data list.   
+#'
+#' @description Used in the...  
+NULL 
+
+###
+### STATUS CODES
+###
+
+#' @name former.words
+#' @docType data
+#' @title Helper data list.  
+#'
+#' @description Used in the gen_status_codes() step.   
+NULL 
+
+#' @name future.words
+#' @docType data
+#' @title Helper data list.  
+#'
+#' @description Used in the gen_status_codes() step.    
+NULL 
+
+#' @name interim.words
+#' @docType data
+#' @title Helper data list.  
+#'
+#' @description Used in the gen_status_codes() step.   
+NULL 
+
+#' @name regional.words
+#' @docType data
+#' @title Helper data list.  
+#'
+#' @description Used in the gen_status_codes() step.   
+NULL 
+
+#' @name possible.regional.list
+#' @docType data
+#' @title Helper data list.  
+#'
+#' @description Used in the gen_status_codes() step.    
 NULL 
