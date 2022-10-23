79137912258eeee7481d52b343d5b044b449ec0a
Kevin Cui <k.cui1234@gmail.com>
2022-10-22 21:02:52 -0400

    documented split titles methodology

--- R/04-split-titles.R
+++ R/04-split-titles.R
@@ -28,28 +28,60 @@ split_titles <- function(df, title = "TitleTxt3"){
   
   df$Num.Titles <- identify_split_num(TitleTxt)
   
-  df$TitleTxt4 <- TitleTxt
+  df$TitleTxt4 <- TitleTxt 
   
-  has.one.title <- df$Num.Titles == 1
-  multiple.titles <- df[df$Num.Titles >= 2, ]
+  
+  #WHAT ARE WE DOING FROM HERE ON??
+  
+  #first pass thru
+  
+  #boolean vector of all titles with only one occurrence
+  has.one.title <- df$Num.Titles == 1 
+  
+  #data frame of entires with at least 2 titles
+  multiple.titles <- df[df$Num.Titles >= 2, ] 
+  
+  #selecting only the first title in titles with multiple possible elements
   df$TitleTxt4 <- ifelse(has.one.title, TitleTxt,
                          substr(TitleTxt,1, regexpr("&",TitleTxt)-1))
+  
+  #saving the one-title filtered df as a temp data frame
   temp <- df
   
   #assuming no field has more than 5 titles
-  for(i in 1:5){
+  max.num.titles <- 5
+  
+  #iterative approach
+  for(i in 1:max.num.titles){
     
     #KEY STEPS
-    has.one.title <- temp$Num.Titles == 1
+    
+    #repeatedly creating a boolean vector reflecting which entries 
+    #have singular titles
+    has.one.title <- temp$Num.Titles == 1 
+    
+    #creating the data frame with multiple titles (for consistency this is duplicated)
     if(i!=1) multiple.titles <- temp[temp$Num.Titles >= 2, ]
+    
+    #selecting only the first title in temp
     temp$TitleTxt4 <- ifelse(has.one.title, TitleTxt, 
                              substr(TitleTxt,1, regexpr("&",TitleTxt)-1))
+    
+    #binding the singular titles to the data frame (if not first step)
     if(i != 1) df <- rbind(df,temp)
+    
+    #decrementing multi-titles title count
     multiple.titles$Num.Titles <- multiple.titles$Num.Titles - 1
     
+    #removing the first title from the multi=title entries
     multiple.titles$TitleTxt4 <- remove_first_split(multiple.titles$TitleTxt4)
     
+    #resetting the temp to the previously multi-title data frame
     temp = multiple.titles
+    #at this point, one layer of titles should have been removed, and
+    #we will iteratively remove until there are only singular titles in temp
+    
+    #resetting titletxt
     TitleTxt <- temp$TitleTxt4
   }
   
