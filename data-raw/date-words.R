2db1e6142f20ee11b3bfe3702965563a016ff3ca
Jesse Lecy <jdlecy@gmail.com>
2022-11-03 12:41:50 -0700

    Create date-words.R

---
+++ data-raw/date-words.R
@@ -0,0 +1,9 @@
+date.words <- 
+  c( "JANUARY","JAN","FEBRUARY","FEB",
+     "MARCH","MAR","APRIL","APR","MAY",
+     "JUNE","JUN","JULY","JUL","AUGUST","AUG",
+     "SEPTEMBER","SEPT","SEP","OCTOBER","OCT",
+     "NOVEMBER","NOV","DECEMBER","DEC", 
+     "PARTIAL YEAR", "PARTIAL YR", "PART YEAR", 
+     "PART-YEAR", "YR", "YEAR", "PART YR",
+     "MO", "MOS", "MONTH", "MONTHS" )
