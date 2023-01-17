2b15bd19efc5117e1d984a8c4caad4f727f3f602
Jesse Lecy <jdlecy@gmail.com>
2023-01-16 18:05:04 -0700

    Update 04-split-titles.R

--- R/04-split-titles.R
+++ R/04-split-titles.R
@@ -13,86 +13,125 @@ require(stringr)
 #' runtime compared to old code is about several magnitudes faster than before
 #' 
 #' @export
-split_titles <- function(df, title = "TitleTxt3"){
+split_titles <- function( df, title = "TitleTxt3" )
+{
+
+  x <- df[[title]]
   
-  TitleTxt <- df[[title]]
+  # final substitutions before split 
+  # MOVE TO PREVIOUS STEP 
+  x <- gsub( "\\d", "", x )
   
-  TitleTxt <- gsub("\\d", "",TitleTxt)
+  x <- gsub( pattern = "\\bEX[A-Z]*\\b\\s*&\\s*DIR[A-Z]*\\b",
+             replacement = "EXECUTIVE DIRECTOR", 
+             x )
   
-  
-  TitleTxt <- gsub("\\bEX[A-Z]*\\b\\s*&\\s*DIR[A-Z]*\\b",
-                   "EXECUTIVE DIRECTOR", TitleTxt)
-  
-  TitleTxt <- gsub("^\\s*SEC[A-Z]*\\s*TREAS[A-Z]*\\b$", 
-                   "SECRETARY & TREASURER", TitleTxt)
+  x <- gsub( pattern = "^\\s*SEC[A-Z]*\\s*TREAS[A-Z]*\\b$", 
+             replacement = "SECRETARY & TREASURER", 
+             x )
  
   # split all FOUNDER titles
-  TitleTxt <- gsub( " & FOUNDER\\b", " FOUNDER", TitleTxt )
-  TitleTxt <- gsub( "\\bFOUNDING\\b", "FOUNDER", TitleTxt )
-  TitleTxt <- gsub( "\\bFOUNDER\\b", "& FOUNDER", TitleTxt )
+  x <- gsub( " & FOUNDER\\b", " FOUNDER", x )
+  x <- gsub( "\\bFOUNDING\\b", "FOUNDER", x )
+  x <- gsub( "\\bFOUNDER\\b", "& FOUNDER", Tx )
   
-  df$Num.Titles <- identify_split_num(TitleTxt)
+  # unique titles have & as separators:
   
-  df$TitleTxt4 <- TitleTxt 
+  title.list <- strsplit( x, "&" )
+  title.list <- lapply( titles, trimws )
   
+  # ADD title veracity check here
+
+  # count number of unique titles per person:
+  title.count <- sapply( title.list, length )
   
-  #WHAT ARE WE DOING FROM HERE ON??
+  # duplicate rows of df based 
+  # on number of unique titles
+  df <- df[  row.count , ]
   
-  #first pass thru
+  # single vector with all unique titles 
+  new.titles.vec <- unlist( title.list )
+  df$TitleTxt4 <- new.titles.vec
   
-  #boolean vector of all titles with only one occurrence
-  has.one.title <- df$Num.Titles == 1 
+  # SANITY CHECK 
+  # length(new.titles.vec) == nrow(df)
   
-  #data frame of entires with at least 2 titles
-  multiple.titles <- df[df$Num.Titles >= 2, ] 
-  
-  #selecting only the first title in titles with multiple possible elements
-  df$TitleTxt4 <- ifelse(has.one.title, TitleTxt,
-                         substr(TitleTxt,1, regexpr("&",TitleTxt)-1))
-  
-  #saving the one-title filtered df as a temp data frame
-  temp <- df
-  
-  #assuming no field has more than 5 titles
-  max.num.titles <- 5
-  
-  #iterative approach
-  for(i in 1:max.num.titles){
-    
-    #KEY STEPS
-    
-    #repeatedly creating a boolean vector reflecting which entries 
-    #have singular titles
-    has.one.title <- temp$Num.Titles == 1 
-    
-    #creating the data frame with multiple titles (for consistency this is duplicated)
-    if(i!=1) multiple.titles <- temp[temp$Num.Titles >= 2, ]
-    
-    #selecting only the first title in temp
-    temp$TitleTxt4 <- ifelse(has.one.title, TitleTxt, 
-                             substr(TitleTxt,1, regexpr("&",TitleTxt)-1))
-    
-    #binding the singular titles to the data frame (if not first step)
-    if(i != 1) df <- rbind(df,temp)
-    
-    #decrementing multi-titles title count
-    multiple.titles$Num.Titles <- multiple.titles$Num.Titles - 1
-    
-    #removing the first title from the multi=title entries
-    multiple.titles$TitleTxt4 <- remove_first_split(multiple.titles$TitleTxt4)
-    
-    #resetting the temp to the previously multi-title data frame
-    temp = multiple.titles
-    #at this point, one layer of titles should have been removed, and
-    #we will iteratively remove until there are only singular titles in temp
-    
-    #resetting titletxt
-    TitleTxt <- temp$TitleTxt4
-  }
+  # number of unique titles per person
+  new.title.id <- sapply( title.count, seq ) %>% unlist()
+  df$num.titles <- new.title.id  
   
   print("split titles step complete")
   return(df)
 }
+  
+
+  
+
+# df$Num.Titles <- identify_split_num(TitleTxt)
+# 
+# df$TitleTxt4 <- TitleTxt 
+# 
+# 
+# #WHAT ARE WE DOING FROM HERE ON??
+# 
+# #first pass thru
+# 
+# #boolean vector of all titles with only one occurrence
+# has.one.title <- df$Num.Titles == 1 
+# 
+# #data frame of entires with at least 2 titles
+# multiple.titles <- df[df$Num.Titles >= 2, ] 
+# 
+# #selecting only the first title in titles with multiple possible elements
+# df$TitleTxt4 <- ifelse(has.one.title, TitleTxt,
+#                        substr(TitleTxt,1, regexpr("&",TitleTxt)-1))
+# 
+# #saving the one-title filtered df as a temp data frame
+# temp <- df
+# 
+# #assuming no field has more than 5 titles
+# max.num.titles <- 5
+# 
+# #iterative approach
+# for(i in 1:max.num.titles){
+#   
+#   #KEY STEPS
+#   
+#   #repeatedly creating a boolean vector reflecting which entries 
+#   #have singular titles
+#   has.one.title <- temp$Num.Titles == 1 
+#   
+#   #creating the data frame with multiple titles (for consistency this is duplicated)
+#   if(i!=1) multiple.titles <- temp[temp$Num.Titles >= 2, ]
+#   
+#   #selecting only the first title in temp
+#   temp$TitleTxt4 <- ifelse(has.one.title, TitleTxt, 
+#                            substr(TitleTxt,1, regexpr("&",TitleTxt)-1))
+#   
+#   #binding the singular titles to the data frame (if not first step)
+#   if(i != 1) df <- rbind(df,temp)
+#   
+#   #decrementing multi-titles title count
+#   multiple.titles$Num.Titles <- multiple.titles$Num.Titles - 1
+#   
+#   #removing the first title from the multi=title entries
+#   multiple.titles$TitleTxt4 <- remove_first_split(multiple.titles$TitleTxt4)
+#   
+#   #resetting the temp to the previously multi-title data frame
+#   temp = multiple.titles
+#   #at this point, one layer of titles should have been removed, and
+#   #we will iteratively remove until there are only singular titles in temp
+#   
+#   #resetting titletxt
+#   TitleTxt <- temp$TitleTxt4
+# }
+# 
+# print("split titles step complete")
+# return(df)
+# }
+
+
+
 
 #' @title 
 #' identify split num function
